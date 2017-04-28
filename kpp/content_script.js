e2f1ff9dd8b8742b715d8f51c13deacabd2c5e29
Spencer West <spencer.west@kbb.com>
2017-04-28 16:55:05 -0700

    version 1.0

--- kpp/content_script.js
+++ kpp/content_script.js
@@ -1,16 +1,6 @@
-// function handleChanges() {
-//   var elem = document.querySelector('.Modal__container');
-//   var firstElem = elem.querySelector('.Button');
-//   firstElem.click();
-// };
-//
-// var observer = new MutationSummary({
-//   callback: handleChanges, // required
-//   queries: [{
-//     element: ".Modal__container"
-//   }]
-// });
-function myfunc(){
-    alert('hi');
-}
-myfunc();
+setInterval(function(){
+  var elem = document.querySelector('.Modal__container');
+  var firstElem = elem.querySelector('.Button');
+  firstElem.click();
+  console.log('clicked button!');
+}, 3000);
