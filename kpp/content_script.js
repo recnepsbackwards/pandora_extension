5ab723b82954757b1303338e08620b2273d55f8f
Spencer West <spencer.west@kbb.com>
2017-04-28 12:31:49 -0700

    updates manifest json

---
+++ kpp/content_script.js
@@ -0,0 +1,16 @@
+// function handleChanges() {
+//   var elem = document.querySelector('.Modal__container');
+//   var firstElem = elem.querySelector('.Button');
+//   firstElem.click();
+// };
+//
+// var observer = new MutationSummary({
+//   callback: handleChanges, // required
+//   queries: [{
+//     element: ".Modal__container"
+//   }]
+// });
+function myfunc(){
+    alert('hi');
+}
+myfunc();
