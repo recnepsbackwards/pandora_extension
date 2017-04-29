bff8e44bd363b0d8c891ec98763d622ca56d3496
Spencer West <spencer.west@kbb.com>
2017-04-28 17:29:52 -0700

    updates logic

--- kpp/content_script.js
+++ kpp/content_script.js
@@ -1,6 +1,8 @@
 setInterval(function(){
-  var elem = document.querySelector('.Modal__container');
-  var firstElem = elem.querySelector('.Button');
-  firstElem.click();
-  console.log('clicked button!');
+  if (document.querySelector('.Modal__container') !== null) {
+    var elem = document.querySelector('.Modal__container');
+    var firstElem = elem.querySelector('.Button');
+    firstElem.click();
+    console.log('clicked button!');
+  }
 }, 3000);
